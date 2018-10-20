60b2acaca51a0255337a1dbad7edd5dcc30f54f0
diarrabell <dbell5@u.rochester.edu>
2018-10-20 19:27:04 -0400

    jhjb

--- Lab4/src/DNAList.java
+++ Lab4/src/DNAList.java
@@ -11,42 +11,41 @@ public class DNAList {
     public static void main(String[] args) {
         DNA test = new DNA();
         File file = new File(args[1]);
-        try(BufferedReader br = new BufferedReader(new FileReader(file))){
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
-            while ((line = br.readLine()) !=null){
-                String [] lineArr = line.split("");
-                switch(lineArr[0]){
-                  case "print":
-                      if (lineArr.length > 1){
-                          String print = test.print(Integer.parseInt(lineArr[1]));
-                          System.out.println(print);
-                      }
-                      else{
-                          String print = test.print();
-                          System.out.println(print);
-                      }
-                      break;
-                  case "remove":
-                      test.remove(Integer.parseInt(lineArr[1]));
-                      break;
-                  case "insert":
-                      test.insert(Integer.parseInt(lineArr[1]), lineArr[2], lineArr[3]);
-                      break;
-                  case "copy":
-                      test.copy(Integer.parseInt(lineArr[1]), Integer.parseInt(lineArr[2]));
-                      break;
-                  case "transcribe":
-                      test.transcribe(Integer.parseInt(lineArr[1]));
-                      break;
-                  case "clip":
-                      test.clip(Integer.parseInt(lineArr[1]), Integer.parseInt(lineArr[2]), Integer.parseInt(lineArr[3]));
-                      break;
-                  default:
-                      break;
-              }
+            while ((line = br.readLine()) != null) {
+                if (line.length() > 0) {
+                    String[] lineArr = line.split("");
+                    switch (lineArr[0]) {
+                        case "print":
+                            if (lineArr.length > 1) {
+                                String print = test.print(Integer.parseInt(lineArr[1]));
+                                System.out.println(print);
+                            } else {
+                                String print = test.print();
+                                System.out.println(print);
+                            }
+                            break;
+                        case "remove":
+                            test.remove(Integer.parseInt(lineArr[1]));
+                            break;
+                        case "insert":
+                            test.insert(Integer.parseInt(lineArr[1]), lineArr[2], lineArr[3]);
+                            break;
+                        case "copy":
+                            test.copy(Integer.parseInt(lineArr[1]), Integer.parseInt(lineArr[2]));
+                            break;
+                        case "transcribe":
+                            test.transcribe(Integer.parseInt(lineArr[1]));
+                            break;
+                        case "clip":
+                            test.clip(Integer.parseInt(lineArr[1]), Integer.parseInt(lineArr[2]), Integer.parseInt(lineArr[3]));
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
