2d65a01513760d60612d4e22b06caa5f75e2e480
diarrabell <dbell5@u.rochester.edu>
2018-10-21 11:28:03 -0400

    Revert "dyhgcgh"
    This reverts commit 18a982a4b47065908260c53bfdd844da94f8ce13.

--- Lab4/src/DNAList.java
+++ Lab4/src/DNAList.java
@@ -9,16 +9,32 @@ import java.io.*;
 
 public class DNAList {
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        DNA test = new DNA(n);
+<<<<<<< HEAD
+        DNA test = new DNA();
         File file = new File(args[1]);
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+=======
+        int n = Integer.parseInt(args[0]);
+        DNA test = new DNA(n);
+        File input = new File(args[1]);
+
+        try(BufferedReader br = new BufferedReader(new FileReader(input))){
+>>>>>>> 5f02ca15ecdaf4bc709f36e0ccee3ff03add3a3c
             String line;
+            int j = 0;
             while ((line = br.readLine()) != null) {
-                if (line.length() > 0) {
-                    String[] lineArr = line.split("");
-                    switch (lineArr[0]) {
-                        case "print":
+                    String[] lineArr = line.split(" ");
+
+                    // ignores extraneous spaces
+                    for(int i=0; i<lineArr.length; i++) {
+                        if (!lineArr[i].isEmpty()) {
+                            j = i;
+                            i = lineArr.length;
+                        }
+                    }
+
+                    switch (lineArr[j]) {
+                        case ("print"):
                             if (lineArr.length > 1) {
                                 String print = test.print(Integer.parseInt(lineArr[1]));
                                 System.out.println(print);
@@ -27,28 +43,27 @@ public class DNAList {
                                 System.out.println(print);
                             }
                             break;
-                        case "remove":
+                        case ("remove"):
                             test.remove(Integer.parseInt(lineArr[1]));
                             break;
-                        case "insert":
+                        case ("insert"):
                             test.insert(Integer.parseInt(lineArr[1]), lineArr[2], lineArr[3]);
                             break;
-                        case "copy":
+                        case ("copy"):
                             test.copy(Integer.parseInt(lineArr[1]), Integer.parseInt(lineArr[2]));
                             break;
-                        case "transcribe":
+                        case ("transcribe"):
                             test.transcribe(Integer.parseInt(lineArr[1]));
                             break;
-                        case "clip":
+                        case ("clip"):
                             test.clip(Integer.parseInt(lineArr[1]), Integer.parseInt(lineArr[2]), Integer.parseInt(lineArr[3]));
                             break;
                         default:
                             break;
                     }
                 }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
         }
     }
 }
